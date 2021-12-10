e38f9ffeb2237fb22a4f1c9e0ac4ff95baf691af
Douglas Ekehammar Schelin <douglas.ekehammar@gmail.com>
2021-12-10 23:26:09 +0100

    Day7 part 1 ok

---
+++ AoC2021Day7/AoC2021Day7/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace AoC2021Day7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var crabs = File.ReadAllText(@"..\..\..\day7.txt").Split(',').Select(x => Convert.ToInt32(x)).ToArray();
+
+            int[] positions = new int[crabs.Max()];
+
+            for (int i = 0; i < crabs.Max(); i++)
+            {
+                for (int o = 0; o < crabs.Length; o++)
+                {
+                    positions[i] += Math.Abs(crabs[o] - i);
+                }
+            }
+
+            var smallest = positions.Where(x => x > 0).Min();
+
+            Console.WriteLine(smallest);
+        }
+    }
+}
+
+//PART 1 - 333755 RIGHT ANSWER, FIRST TRY
