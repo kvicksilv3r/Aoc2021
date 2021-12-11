03de7cc6959f8aff0ae93d0d8bf720af638315a6
Douglas Ekehammar Schelin <douglas.ekehammar@gmail.com>
2021-12-11 04:20:53 +0100

    Day 8 part 1 bruteforce lol

---
+++ AoC2021Day8/AoC2021Day8/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace AoC2021Day8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var inputs = File.ReadAllLines(@"..\..\..\day8.txt");
+            int counter = 0;
+            var matches = new int[] { 2, 4, 3, 7 };
+
+            foreach (var input in inputs)
+            {
+                var s = input.Split('|')[1];
+                var p = s.Split(' ');
+
+                for (int i = 0; i < p.Length; i++)
+                {
+                    if (p[i] != "" || p[i] != " ")
+                    {
+                        if (matches.Contains(p[i].Length))
+                        {
+                            counter++;
+                        }
+                    }
+                }
+            }
+            Console.WriteLine(counter);
+        }
+    }
+}
